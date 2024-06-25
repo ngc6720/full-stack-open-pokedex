84f4ceba596597eea74217f6fe7d78a1adf366cd
ngc6720 <francoiskerforn@gmail.com>
2024-06-25 22:18:14 +0200

    updates

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
 
-app.get("/version", (req, res) => {
-  res.send("9"); // change this string to ensure a new version deployed
-});
-
 app.get("/healthz", (req, res) => {
   res.send("ok");
 });
