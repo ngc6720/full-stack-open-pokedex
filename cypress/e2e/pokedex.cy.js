c258cd4081188d74fb381a73248cf26e8b02de30
ngc6720 <francoiskerforn@gmail.com>
2024-06-25 14:17:17 +0200

    actions

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,17 @@
+const PORT = 3000;
+
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:5000");
+    cy.visit("http://localhost:" + PORT);
     cy.contains("ivysaur");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
+  });
+
+  it("pokemon page can be opened", function () {
+    cy.visit("http://localhost:" + PORT);
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
   });
 });
